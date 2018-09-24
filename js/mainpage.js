5d0ba99405ada8a2f210a7e0919ea6e628c4ce50
timetrp <js_time123@hotmail.com>
2018-09-24 17:12:32 +0700

    mouse over toggle class

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,1 +1,45 @@
-djskfj
+var tribeName = document.querySelectorAll(".tribe-name");
+var tribeContent = document.querySelectorAll(".tribe-content");
+
+console.log(tribeName);
+
+
+tribeName.forEach(function(name,index){
+
+    name.addEventListener("mouseover", function(e){
+        $( this ).toggleClass( "hide" );
+        console.log($(tribeContent[index]));
+        $(tribeContent[index]).toggleClass("hide");
+    });
+
+
+      
+
+    
+
+
+
+});
+
+tribeContent.forEach(function(content,index){
+    content.addEventListener("mouseout", function(e){
+
+        $( this ).toggleClass( "hide" );
+
+        $(tribeName[index]).toggleClass("hide");
+
+    });
+
+
+})
+    
+
+  
+
+
+
+
+
+
+
+
