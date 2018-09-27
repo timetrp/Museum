fc8978c92237bc523930f8cd7bfb8f83ee4187b0
purinat <purinat.san@student.mahidol.ac.th>
2018-09-27 21:13:01 +0700

    Template 3 new Homepage

---
+++ js/template3.js
@@ -0,0 +1,20 @@
+
+
+// var but1 = document.querySelector("#side-bar-button-1");
+// var but2 = document.querySelector("#side-bar-button-2");
+// var side = document.querySelector(".side");
+
+
+// but1.addEventListener("click",function(){
+
+//     // side.classList.remove("side-bar2");
+//     side.classList.toggle("side-bar1");
+    
+// });
+
+// // but2.addEventListener("click",function(){
+
+// //     side.classList.remove("side-bar1");
+// //     side.classList.add("side-bar2");
+    
+// // });
