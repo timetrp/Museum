000bd67ccac1acb78c2348534aac4b2b66e05f90
timetrp <js_time123@hotmail.com>
2018-10-02 19:02:20 +0700

    responsive collapse navbar with animation

--- js/template3.js
+++ js/template3.js
@@ -18,3 +18,20 @@
 // //     side.classList.add("side-bar2");
     
 // // });
+
+
+$(document).ready ( function(){
+
+    var dropDown = document.querySelector('#collapse-nav');
+    console.log(dropDown);
+    $( "#toggle-drop-down" ).click(function() {
+        console.log('clicked');
+        console.log($('#collapse-nav'));
+        $( "#collapse-nav" ).toggleClass( 'newbar' );
+
+      });
+    
+
+
+ });
+
