8f42535c6d016c7dd8a25dc85ec351db6c59482a
timetrp <js_time123@hotmail.com>
2018-10-11 12:30:05 +0700

    fix bug

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,46 +1,65 @@
 var tribeName = document.querySelectorAll(".tribe-name");
 var tribeContent = document.querySelectorAll(".tribe-content");
+var tabLink = document.querySelectorAll(".tribe-name a");
 
 var isShowing = Array.from(tribeName).map(() => false);
+
+
+
+tabLink.forEach(function (link){
+
+
+    link.addEventListener("mouseclick", function(e){
+        if (e.target !== this) {
+            return;
+        }
+
+        if (!isShowing[e.target.accessKey]) {
+            jQuery(this).fadeOut("slow", () => {
+                $(this.nextElementSibling).css("display", "flex")
+                .fadeIn("slow", () => {
+                    
+                });
+            });
+        }
+
+    })
+
+
+
+});
+
+
 
 
 tribeName.forEach(function (name, index) {
 
     name.addEventListener("mouseover", function (e, index) {
 
-
-
         if (e.target !== this) {
             return;
         }
 
         if (!isShowing[e.target.accessKey]) {
-            // console.log(e);
-            // console.log(this.nextElementSibling);
-            // $( this ).addClass("fadeout");
-            // $( this ).addClass( "hide" );
-
             jQuery(this).fadeOut("slow", () => {
                 $(this.nextElementSibling).css("display", "flex")
                 .fadeIn("slow", () => {
-
                     setTimeout(function () {
-
-
                         $(this.nextElementSibling).fadeOut("slow", () => {
                             // console.log(this);
                             $(this).css("display", "flex")
                             .fadeIn("slow");
                         })
-
-
-
                     }.bind(this), 10000);
                 });
             });
         }
     });
-
 });
+
+
+
+
+
 
 
